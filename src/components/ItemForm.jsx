abaea019a9fd3f7c9a3093dfe58ff22072fc1e5e
David Fall <david@madebycodera.com>
2020-09-18 02:41:26 -0700

    SCA - Guard against entering price that is 0 or less for an item

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -26,13 +26,20 @@ class ItemForm extends React.Component {
           <div className="form-group">
             <label htmlFor={ITEM_PRICE_FIELD}>Item Price</label>
             <input
-              required
-              type="number"
-              className="form-control"
-              id={ITEM_PRICE_FIELD}
-              placeholder="Enter price"
-              value={this.state.itemPrice}
-              onChange={(event) => this.handleFieldChange(ITEM_PRICE_FIELD, event.target.value)}
+            required
+            type="number"
+            className="form-control"
+            id={ITEM_PRICE_FIELD}
+            placeholder="Enter price"
+            value={this.state.itemPrice}
+            onChange={(event) => {
+              const value = event.target.value
+              if (value !== '' && value <= 0) {
+                alert('Please enter a number greater than 0 for the item price!')
+                return
+              }
+              this.handleFieldChange(ITEM_PRICE_FIELD, value)
+            }}
             />
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
