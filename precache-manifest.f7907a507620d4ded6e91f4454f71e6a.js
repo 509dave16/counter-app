7f6a7502460bdcb8b94231735a8ad99ef1285311
David Fall <david@madebycodera.com>
2020-09-18 02:13:11 -0700

    Updates

---
+++ precache-manifest.f7907a507620d4ded6e91f4454f71e6a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "75556171f3d6190f63b5",
+    "url": "/counter-app/static/css/main.00e924ec.chunk.css"
+  },
+  {
+    "revision": "75556171f3d6190f63b5",
+    "url": "/counter-app/static/js/main.f2a78d35.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "29fff62f45868cd296ff",
+    "url": "/counter-app/static/css/2.1015ebfc.chunk.css"
+  },
+  {
+    "revision": "29fff62f45868cd296ff",
+    "url": "/counter-app/static/js/2.d0b98241.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a9fd02529e1109cce6df3baafae2add7",
+    "url": "/counter-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/counter-app/precache-manifest.dbfd1f81d8b3c759d65365ab6bf6c437.js"
+  "/counter-app/precache-manifest.f7907a507620d4ded6e91f4454f71e6a.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/counter-app/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
